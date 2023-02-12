87dfb50e02dab9c9a75c131f2de01751094dc528
Zaacky24 <zaacky98@gmail.com>
2023-02-11 16:57:26 -0800

    5-base_geometry.py

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# 5-base_geometry.py
+""" File name : 5-base_geometry.py
+    It is not allowed to import any module
+"""
+
+
+class BaseGeometry(object):
+    """Define class base geometry"""
+    pass
